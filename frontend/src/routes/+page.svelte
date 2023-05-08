86024427e55f1a70b6135a9a2e7d30e457ab710f
Vivek Kaushik <vk@Viveks-MacBook-Air.local>
2023-05-09 03:16:50 +0530

    Added comments and seperated file

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,14 +2,17 @@
   import { onMount } from 'svelte';
   import { createEventDispatcher } from 'svelte';
 
+  // Initialising variables
   let playerOneScore = 0;
   let playerTwoScore = 0;
   let winner = '';
   let lastPlayer = null;
   const dispatch = createEventDispatcher();
 
+  // Function to add a point to a player
   async function addPoint(player) {
   try {
+    // Send a request to the server to update the score
     const response = await fetch("http://localhost:5000/update_score", {
       method: "POST",
       headers: {
@@ -19,6 +22,7 @@
     });
     const data = await response.json();
 
+    // Update based on request
     if (data.success) {
       if (data.player === 1) {
         playerOneScore += 1;
@@ -31,9 +35,12 @@
     console.error("Error updating score:", error);
   }
 }
+
+// Function to delete the previous point
 async function deletePreviousPoint() {
     if (lastPlayer) {
       try {
+        // Send a request to the server to delete the previous point
         const response = await fetch("http://localhost:5000/delete_previous_point", {
           method: "POST",
           headers: {
@@ -44,6 +51,7 @@ async function deletePreviousPoint() {
 
         const data = await response.json();
 
+        // Update based on request
         if (data.success) {
           if (data.player === 1 && playerOneScore > 0) {
             playerOneScore -= 1;
@@ -58,14 +66,17 @@ async function deletePreviousPoint() {
     }
   }
 
+  // Check for a winner 
   $: winner = playerOneScore >= 21 ? 'Player 1' : playerTwoScore >= 21 ? 'Player 2' : '';
 
+  // Function to reset the game
   function resetGame() {
     playerOneScore = 0;
     playerTwoScore = 0;
     winner = '';
   }
 
+  // Show the modal when there is a winner
   onMount(() => {
     if (winner) {
       dispatch('show-modal');
@@ -73,31 +84,43 @@ async function deletePreviousPoint() {
   });
 </script>
 
+<!-- Main content -->
 <main>
   <h1>Badminton Score</h1>
+
   <!-- svelte-ignore a11y-img-redundant-alt -->
   <img style="margin-bottom: 1rem;" src="/badminton_racket.jpg" alt="Badminton Image" width="450" height="450">
+  
   <div class="inputs">
+    <!-- Input forms for player names -->
     <form style="margin-right: 2rem;">
       <label for="fname">Player 1 Name:</label><br>
       <input type="text" id="p1name" name="p1name"><br>
     </form>
+
     <form>
       <label for="fname">Player 2 Name:</label><br>
       <input type="text" id="p2name" name="p2name"><br>
     </form>
   </div>
+
+  <!-- Display scores and buttons to add points for each player -->
   <div class="scoreboard">
     <div class="player player-one">
       <button style="margin-left: 2.5rem;" on:click={() => addPoint(1)}>+</button>
       <p style="margin-left: 2.5rem;">{playerOneScore}</p>
     </div>
+
     <div class="player player-two">
       <button style="margin-right: 2.5rem;" on:click={() => addPoint(2)}>+</button>
       <p style="margin-right: 2.5rem;" >{playerTwoScore}</p>
     </div>
   </div>
+
+  <!-- Button to delete the previous point -->
   <button style="margin-bottom: 2rem;" on:click={() => deletePreviousPoint()}>Delete previous point</button>
+  
+  <!-- Display the winner and a button to reset the game when there is a winner -->
   {#if winner}
   <div
   class="modal-overlay"
@@ -111,6 +134,7 @@ async function deletePreviousPoint() {
   role="button" 
   aria-label="Reset game"
 >
+
       <div class="modal">
         <h2>{winner} wins!</h2>
         <button on:click={() => resetGame()}>Reset</button>
@@ -119,55 +143,7 @@ async function deletePreviousPoint() {
   {/if}
 </main>
 
+<!-- Import the styles -->
 <style>
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: beige;
-  }
-
-  .scoreboard {
-    display: flex;
-    justify-content: space-between;
-    width: 300px;
-    align-items: center;
-  }
-
-  .inputs {
-    display: flex;
-    margin-bottom: 2rem;
-    font-weight: bold;
-  }
-
-  .player {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .player p {
-    font-size: 2rem;
-  }
-
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .modal {
-    background-color: white;
-    padding: 1rem;
-    border-radius: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+  @import './scoreboard.css';
 </style>
